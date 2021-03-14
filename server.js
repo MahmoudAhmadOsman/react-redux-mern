9dafe5e6273194baa8d4de476740291db4987f6f
Mahmoud Osman <34916796+MahmoudAhmadOsman@users.noreply.github.com>
2021-03-13 23:43:06 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,36 +1,27 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const morgan = require("morgan");
 const shortid = require("shortid");
-const path = require("path");
+
 const app = express();
 app.use(bodyParser.json());
 
-
-
 app.use("/", express.static(__dirname + "/build"));
-//app.get("/", (req, res) => res.sendFile(__dirname + "/build/index.html"));
+app.get("/", (req, res) => res.sendFile(__dirname + "/build/index.html"));
 
+mongoose.connect(
+  process.env.MONGODB_URL || "mongodb://localhost/react-shopping-cart-db",
+  {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  }
+);
 
-
-//Routes
-// const routes = require("./routes/api");
-
-//Database connection string
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/mongodb2020", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-//
-//Product Model or schema
 const Product = mongoose.model(
   "products",
   new mongoose.Schema({
-    id: {
-      type: String,
-      default: shortid.generate,
-    },
+    _id: { type: String, default: shortid.generate },
     title: String,
     description: String,
     image: String,
@@ -39,41 +30,37 @@ const Product = mongoose.model(
   })
 );
 
-//Products End point api
 app.get("/api/products", async (req, res) => {
   const products = await Product.find({});
   res.send(products);
 });
 
-// //Create a new product
 app.post("/api/products", async (req, res) => {
   const newProduct = new Product(req.body);
   const savedProduct = await newProduct.save();
   res.send(savedProduct);
 });
 
-// //Delete product
 app.delete("/api/products/:id", async (req, res) => {
   const deletedProduct = await Product.findByIdAndDelete(req.params.id);
   res.send(deletedProduct);
 });
 
-//Order  Modal or table
 const Order = mongoose.model(
   "order",
   new mongoose.Schema(
     {
-      id: {
+      _id: {
         type: String,
         default: shortid.generate,
       },
-      name: String,
       email: String,
+      name: String,
       address: String,
       total: Number,
       cartItems: [
         {
-          id: String,
+          _id: String,
           title: String,
           price: Number,
           count: Number,
@@ -86,7 +73,6 @@ const Order = mongoose.model(
   )
 );
 
-//Order POST API
 app.post("/api/orders", async (req, res) => {
   if (
     !req.body.name ||
@@ -95,38 +81,19 @@ app.post("/api/orders", async (req, res) => {
     !req.body.total ||
     !req.body.cartItems
   ) {
-    return res.send({ message: "All fields are required!!" });
+    return res.send({ message: "Data is required." });
   }
-
-  //Save the order into the database
   const order = await Order(req.body).save();
-  //Now, send the order
+  res.send(order);
+});
+app.get("/api/orders", async (req, res) => {
+  const orders = await Order.find({});
+  res.send(orders);
+});
+app.delete("/api/orders/:id", async (req, res) => {
+  const order = await Order.findByIdAndDelete(req.params.id);
   res.send(order);
 });
 
-//HTTP request logger
-app.use(morgan("tiny"));
-//All posts
-// app.use("/api/posts", postsRouter);
-// app.use("/api", routes);
-
-//Check if the connection variable //3
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-  
-  //3/13
-   app.get('*', (req, res) => {
-       res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
-
 const port = process.env.PORT || 5000;
-const server = app.listen(port, () =>
-  console.log("Serve started at http://localhost:5000")
-);
-
-//Show/handle the server error in better way
-process.on("unhandledRejection", (err, promise) => {
-  console.log(`Type of Logged Error: ${err}`);
-  server.close(() => process.exit(1));
-});
+app.listen(port, () => console.log("serve at http://localhost:5000"));
